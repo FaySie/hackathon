e3e8c36f61b04a1fee7bed2f9a6726f0e141f98b
supergud <supergud3@gmail.com>
2017-07-30 10:49:15 +0800

    update task view and update data

--- src/Front/Templates/home/home.blade.php
+++ src/Front/Templates/home/home.blade.php
@@ -30,12 +30,14 @@
         <div class="swiper-container">
             <!-- Additional required wrapper -->
             <div class="swiper-wrapper">
+
                 <!-- Slides -->
                 @foreach ($tags as $tag)
                     <div class="swiper-slide" style="background-image: linear-gradient(295deg, #88aaff, rgba(0, 199, 255, 0.82))">
                         {{ $tag }}
                     </div>
                 @endforeach
+
             </div>
 
             <!-- If we need navigation buttons -->
@@ -49,55 +51,15 @@
             <div class="row">
 
                 <?php
-                    $numColumns = 4;
+                    $numColumns = 3;
                     $span = ceil(12 / $numColumns);
                     $total = count($items);
                 ?>
 
                 @foreach ($items as $i => $item)
                     <div class="col-sm-{{ $span }}  m-b-50">
-                        <div class="add-favorite" data-id="274" data-type="lesson" data-toggle="tooltip" title="" data-original-title="**关注此课程**">
-                            <svg viewBox="0 0 32 32" fill="#FF5A5F" fill-opacity="1" stroke="#FF5A5F" stroke-width="1.5" aria-hidden="true" role="presentation" stroke-linecap="round" stroke-linejoin="round" style="height: 24px; width: 24px; display: block;">
-                                <path d="M23.993 2.75c-.296 0-.597.017-.898.051-1.14.131-2.288.513-3.408 1.136-1.23.682-2.41 1.621-3.688 2.936-1.28-1.316-2.458-2.254-3.687-2.937-1.12-.622-2.268-1.004-3.41-1.135a7.955 7.955 0 0 0-.896-.051C6.123 2.75.75 4.289.75 11.128c0 7.862 12.238 16.334 14.693 17.952a1.004 1.004 0 0 0 1.113 0c2.454-1.618 14.693-10.09 14.693-17.952 0-6.84-5.374-8.378-7.256-8.378"></path>
-                            </svg>
-                        </div>
-                        <div class="card-layout">
-                            <div class="card-thumbnail float">
-                                <img src="{{ $item->image }}" alt="{{ $item->title }}">
-                                <div class="project-intro">
-                                    {!! $item->project_description !!}
 
-                                    <a href="#" class="project-title">
-                                        Project:{{ $item->project_title }}
-                                    </a>
-                                </div>
-                                <div class="card-info">
-                                    <span class="m-r-10">
-                                        <i class="fa fa-eye"></i>
-                                        {{ $item->hits }}
-                                    </span>
-                                    <span class="m-r-10">
-                                        <i class="fa fa-heart"></i>
-                                        {{ $item->likes }}
-                                    </span>
-                                    <span class="star-score">
-                                        <i class="fa fa-star"></i>
-                                        {{ $item->awesome }} Awesome!
-                                    </span>
-                                </div>
-                            </div>
-                            <a href="{{ $router->route('task', ['id' => $item->id]) }}" class="card-content">
-                                <h4 class="bold">
-                                    {{ $item->title }}
-                                </h4>
-                            </a>
-                            <div class="project-user">
-                                 <span class="thumbnail-wrapper d32 circular inline">
-                                    <img src="{{ $item->user_avatar }}" alt="" data-src="{{ $user->user_avatar }}" data-src-retina="{{ $user->user_avatar }}" width="32" height="32" class="hoverZoomLink">
-                                </span>
-                                <div class="establish-user">{{ $item->user_name }}</div>
-                            </div>
-                        </div>
+                        @widget('widget.task-card', ['item' => $item], 'edge')
 
                         <span class="row-separator"></span>
                         <!-- LINE END -->
--- src/Front/Templates/task/task.blade.php
+++ src/Front/Templates/task/task.blade.php
@@ -36,26 +36,26 @@
                     </div>
                     <div class="col-sm-9">
                         <div class="task-des">
-                            <p>{!! $item->description !!}</p>
-                            <button class="start-test btn btn-defaut">Play</button>
+                            {!! $item->description !!}
+
+                            <a href="@route('test', ['id' => $item->id])" class="start-test btn btn-default">Play</a>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="project-info">
                 <h2>
-                    Project Title
+                    {{ $item->project->title }}
                 </h2>
                 <div class="des">
-                    project description project description project description project description project description project description project description project description project description
+                    {!! $item->project->description !!}
                 </div>
                 <div class="tasks">
                     <div class="row">
                         <div class="col-sm-10">
-                            <a class="task btn btn-defaut">Task 1</a>
-                            <a class="task btn btn-defaut">Task 2</a>
-                            <a class="task btn btn-defaut">Task 3</a>
-                            <a class="task btn btn-defaut">Task 4</a>
+                            @foreach($item->project->tasks as $task)
+                                <a href="@route('task', ['id' => $task->id])" class="task btn btn-default">{{ $task->title }}</a>
+                            @endforeach
                         </div>
                         <div class="col-sm-2">
                             <a href="#" class="fa fa-arrow-right more">More...</a>
@@ -68,45 +68,28 @@
                     Related Tasks
                 </h2>
                 <div class="row">
-                    @for ($i = 1; $i<=5 ; $i++)
-                        <div class="col-sm-4 m-b-30">
-                            <div class="add-favorite" data-id="274" data-type="lesson" data-toggle="tooltip" title="" data-original-title="加入我的最愛">
-                                <svg viewBox="0 0 32 32" fill="#FF5A5F" fill-opacity="1" stroke="#FF5A5F" stroke-width="1.5" aria-hidden="true" role="presentation" stroke-linecap="round" stroke-linejoin="round" style="height: 24px; width: 24px; display: block;">
-                                    <path d="M23.993 2.75c-.296 0-.597.017-.898.051-1.14.131-2.288.513-3.408 1.136-1.23.682-2.41 1.621-3.688 2.936-1.28-1.316-2.458-2.254-3.687-2.937-1.12-.622-2.268-1.004-3.41-1.135a7.955 7.955 0 0 0-.896-.051C6.123 2.75.75 4.289.75 11.128c0 7.862 12.238 16.334 14.693 17.952a1.004 1.004 0 0 0 1.113 0c2.454-1.618 14.693-10.09 14.693-17.952 0-6.84-5.374-8.378-7.256-8.378"></path>
-                                </svg>
-                            </div>
-                            <a href="{{ $router->route('home', ['id' => $item->id]) }}" class="card-layout">
-                                <div class="card-thumbnail">
-                                    <img src="{{ $item->image }}" alt="Accusamus.">
-                                    <div class="project-intro">
-                                        test test test test test test
-                                        <span class="project-title">
-                                     test test test test test test
-                                 </span>
-                                    </div>
-                                </div>
-                                <div class="card-content">
-                                    <h5 class="dotdotdot" data-lines="2" style="word-wrap: break-word;">
-                                        {{ $item->title }}
-                                    </h5>
-                                </div>
-                                <div class="card-info">
-                            <span>
-                              <i class="pg pg-clock"></i>
-                              210
-                            </span>
-                                    <span>
-                                <i class="fa fa-user"></i>
-                                508
-                             </span>
-                                    <span class="star-score">
-                                 <i class="fa fa-user"></i>
-                                 508
-                             </span>
-                                </div>
-                            </a>
+
+		            <?php
+		            $numColumns = 3;
+		            $span = ceil(12 / $numColumns);
+		            $total = count($relateds);
+		            ?>
+
+                    @foreach ($relateds as $i => $related)
+                        <div class="col-sm-{{ $span }}  m-b-50">
+
+                            @widget('widget.task-card', ['item' => $related], 'edge')
+
+                            <span class="row-separator"></span>
+                            <!-- LINE END -->
                         </div>
-                    @endfor
+
+                        @if($i % $numColumns == $numColumns - 1 || $i == $total - 1)
+                            </div><div class="row">
+                        @endif
+
+                    @endforeach
+
                 </div>
             </div>
         </div>
